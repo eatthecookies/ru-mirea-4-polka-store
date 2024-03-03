ce3ec3633d229295e3951b0d4d08f3ec7540c884
eatthecookies <79151974+eatthecookies@users.noreply.github.com>
2024-03-03 22:54:26 +0300

    Добавление декораторов для доступа ко всем страницам сайта

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return "index"
+    return render_template('index.html', title="Главная страница")
+
+@app.route("/about")
+def about():
+    return render_template('aboutUs.html', title="Про нас")
+@app.route("/gallery")
+def gallery():
+    return render_template('gallery.html', title="Галерея")
+
+@app.route("/catalog")
+def catalog():
+    return render_template('katalog.html', title="Каталог")
+
+@app.route("/registration")
+def registration():
+    return render_template('registration.html', title="Регистрация")
+
+@app.route("/trash")
+def trash():
+    return render_template('trash.html', title="Корзина")
 
 
 if __name__ == "__main__":
